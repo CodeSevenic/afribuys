2bc4c7802df2c5cdd76c8937b6c88abaaa822a62
CodeSevenic <sbu.audio.production@gmail.com>
2021-05-04 12:40:41 +0200

    my changes

--- afribuys-backend/src/routes/userRoutes.js
+++ afribuys-backend/src/routes/userRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const { signup } = require('../controllers/userController');
 
+router.post('/signup', signup);
 router.post('/signin', (req, res) => {});
-
-router.post('/signup', (req, res) => {});
 
 module.exports = router;
