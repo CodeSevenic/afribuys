f58d6a277b121d87054fb05d5fe5e5206352b7fc
CodeSevenic <sbu.audio.production@gmail.com>
2021-05-06 14:42:41 +0200

    my changes

--- afribuys-backend/src/models/cartModel.js
+++ afribuys-backend/src/models/cartModel.js
@@ -7,6 +7,17 @@ const cartSchema = new mongoose.Schema(
       ref: 'User',
       required: true,
     },
+    cartItems: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Product',
+          required: true,
+        },
+        quantity: { type: Number, default: 1 },
+        price: { type: Number, required: true },
+      },
+    ],
   },
   { timestamps: true }
 );
