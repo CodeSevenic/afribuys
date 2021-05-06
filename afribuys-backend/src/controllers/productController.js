dd8c6a3dffc5e71d963bc818815239965d67ae20
CodeSevenic <sbu.audio.production@gmail.com>
2021-05-06 12:52:09 +0200

    my changes

--- afribuys-backend/src/controllers/productController.js
+++ afribuys-backend/src/controllers/productController.js
@@ -1,5 +1,25 @@
 const Product = require('../models/productModel');
+const shortid = require('shortid');
+const slugify = require('slugify');
 
 exports.createProduct = (req, res) => {
-  res.status(200).json({ file: req.files, body: req.body });
+  // res.status(200).json({ file: req.files, body: req.body });
+  const {
+    name,
+    price,
+    description,
+    productPictures,
+    category,
+    createdBy,
+  } = req.body;
+
+  const product = new Product({
+    name: name,
+    slug: slugify(name),
+    price,
+    description,
+    productPictures,
+    category,
+    createdBy,
+  });
 };
