4abd2f52210dd0a7f0033948bf45685cfdd89321
CodeSevenic <sbu.audio.production@gmail.com>
2021-05-06 13:07:24 +0200

    my changes

--- afribuys-backend/src/controllers/productController.js
+++ afribuys-backend/src/controllers/productController.js
@@ -4,14 +4,14 @@ const slugify = require('slugify');
 
 exports.createProduct = (req, res) => {
   // res.status(200).json({ file: req.files, body: req.body });
-  const {
-    name,
-    price,
-    description,
-    productPictures,
-    category,
-    createdBy,
-  } = req.body;
+  const { name, price, description, category, createdBy } = req.body;
+  let productPictures = [];
+
+  if (req.files.length > 0) {
+    productPictures = req.files.map((file) => {
+      return { img: file.filename };
+    });
+  }
 
   const product = new Product({
     name: name,
@@ -20,6 +20,13 @@ exports.createProduct = (req, res) => {
     description,
     productPictures,
     category,
-    createdBy,
+    createdBy: req.user._id,
+  });
+
+  product.save((error, product) => {
+    if (error) return res.status(400).json({ error });
+    if (product) {
+      res.status(201).json({ product });
+    }
   });
 };
