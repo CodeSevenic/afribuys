631c742033a47580e4727e2e1c5517880572d14c
CodeSevenic <sbu.audio.production@gmail.com>
2021-05-07 14:50:42 +0200

    my changes

--- admin-app/src/components/Header/Header.js
+++ admin-app/src/components/Header/Header.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Link to="/" className="navbar-brand">
-        Admin Dashboard
-      </Link>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="mr-auto">
-          {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+      <Container>
+        <Link to="/" className="navbar-brand">
+          Admin Dashboard
+        </Link>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Another action
@@ -22,20 +23,21 @@ const Header = () => {
                 Separated link
               </NavDropdown.Item>
             </NavDropdown> */}
-        </Nav>
-        <Nav>
-          <li className="nav-item">
-            <Link className="nav-link" to="/signin">
-              Sign In
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/signup" className="nav-link">
-              Sign Up
-            </Link>
-          </li>
-        </Nav>
-      </Navbar.Collapse>
+          </Nav>
+          <Nav>
+            <li className="nav-item">
+              <Link className="nav-link" to="/signin">
+                Sign In
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/signup" className="nav-link">
+                Sign Up
+              </Link>
+            </li>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 };
