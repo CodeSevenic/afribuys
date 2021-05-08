c216c13d854989244a55d6b88dd93ce33cc61df5
CodeSevenic <sbu.audio.production@gmail.com>
2021-05-08 16:30:06 +0200

    my changes

--- admin-app/src/actions/authActions.js
+++ admin-app/src/actions/authActions.js
@@ -4,6 +4,7 @@ import { authConstants } from './constants';
 export const login = (user) => {
   console.log(user);
   return async (dispatch) => {
+    dispatch({ type: authConstants.LOGIN_REQUEST });
     const res = await axios.post('/admin/signin', {
       ...user,
     });
--- admin-app/src/reducers/authReducers.js
+++ admin-app/src/reducers/authReducers.js
@@ -1,14 +1,22 @@
 import { authConstants } from '../actions/constants';
 
 const initialState = {
-  name: 'Sibusiso',
+  token: null,
+  user: {
+    name: '',
+    surname: '',
+    email: '',
+    picture: '',
+  },
+  authenticate: false,
+  authenticating: false,
 };
 
 export const loginReducer = (state = initialState, action) => {
   console.log(action);
   switch (action.type) {
     case authConstants.LOGIN_REQUEST:
-      return (state = { ...state, ...action.payload });
+      return (state = { ...state, authenticating: true });
 
     default:
       return state;
