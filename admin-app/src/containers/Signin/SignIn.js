f7b42a9c79b1c26bbbdf859bd2581d0d5f971ba8
CodeSevenic <sbu.audio.production@gmail.com>
2021-05-09 16:07:11 +0200

    my changes

--- admin-app/src/containers/Signin/SignIn.js
+++ admin-app/src/containers/Signin/SignIn.js
@@ -15,7 +15,9 @@ const SignIn = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    if (!loginState.authenticate) dispatch(isUserLoggedIn);
+    if (!loginState.authenticate) {
+      dispatch(isUserLoggedIn);
+    }
   }, [dispatch, loginState.authenticate]);
 
   const userLogin = (e) => {
@@ -25,40 +27,40 @@ const SignIn = () => {
   };
 
   if (loginState.authenticate) {
-    return <Redirect to="/" />;
+    return <Redirect to={'/'} />;
+  } else {
+    return (
+      <Layout>
+        <Container>
+          <Row style={{ marginTop: '50px' }}>
+            <Col md={{ span: 6, offset: 3 }}>
+              <Form onSubmit={userLogin}>
+                <Input
+                  label="Email"
+                  placeholder="Email"
+                  value={email}
+                  type="email"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+
+                <Input
+                  label="Password"
+                  placeholder="Password"
+                  value={password}
+                  type="password"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+
+                <Button variant="primary" type="submit">
+                  Submit
+                </Button>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
+      </Layout>
+    );
   }
-
-  return (
-    <Layout>
-      <Container>
-        <Row style={{ marginTop: '50px' }}>
-          <Col md={{ span: 6, offset: 3 }}>
-            <Form onSubmit={userLogin}>
-              <Input
-                label="Email"
-                placeholder="Email"
-                value={email}
-                type="email"
-                onChange={(e) => setEmail(e.target.value)}
-              />
-
-              <Input
-                label="Password"
-                placeholder="Password"
-                value={password}
-                type="password"
-                onChange={(e) => setPassword(e.target.value)}
-              />
-
-              <Button variant="primary" type="submit">
-                Submit
-              </Button>
-            </Form>
-          </Col>
-        </Row>
-      </Container>
-    </Layout>
-  );
 };
 
 export default SignIn;
