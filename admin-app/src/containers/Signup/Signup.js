9a525f04017c4fccdf784f9e3d9a207b602f2ee7
CodeSevenic <sbu.audio.production@gmail.com>
2021-05-10 09:34:55 +0200

    my changes

--- admin-app/src/containers/Signup/Signup.js
+++ admin-app/src/containers/Signup/Signup.js
@@ -1,30 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from '../../components/Layout/Layout';
 import { Button, Col, Container, Form, Row } from 'react-bootstrap';
 import Input from '../../components/UI/Input/Input';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Redirect } from 'react-router';
+import { signup } from '../../actions/actionsIndex';
 
 const Signup = () => {
+  const [name, setName] = useState('');
+  const [surname, setSurname] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
   const loginState = useSelector((state) => state.loginState);
+
+  const dispatch = useDispatch();
+
+  const userSignup = (e) => {
+    e.preventDefault();
+    const user = {
+      name,
+      surname,
+      email,
+      password,
+    };
+    dispatch(signup(user));
+  };
 
   if (loginState.authenticate) {
     return <Redirect to={'/'} />;
   }
+
   return (
     <Layout>
       <Container>
         <Row style={{ marginTop: '50px' }}>
           <Col md={{ span: 6, offset: 3 }}>
-            <Form>
+            <Form onSubmit={userSignup}>
               <Row>
                 <Col md={6}>
                   <Input
                     label="First Name"
                     placeholder="First name"
-                    value=""
+                    value={name}
                     type="text"
-                    onChange={() => {}}
+                    onChange={(e) => setName(e.target.value)}
                   />
                 </Col>
 
@@ -32,9 +52,9 @@ const Signup = () => {
                   <Input
                     label="Last Name"
                     placeholder="Last name"
-                    value=""
+                    value={surname}
                     type="text"
-                    onChange={() => {}}
+                    onChange={(e) => setSurname(e.target.value)}
                   />
                 </Col>
               </Row>
@@ -42,17 +62,17 @@ const Signup = () => {
               <Input
                 label="Email"
                 placeholder="Email"
-                value=""
+                value={email}
                 type="email"
-                onChange={() => {}}
+                onChange={(e) => setEmail(e.target.value)}
               />
 
               <Input
                 label="Password"
                 placeholder="Password"
-                value=""
+                value={password}
                 type="password"
-                onChange={() => {}}
+                onChange={(e) => setPassword(e.target.value)}
               />
 
               <Button variant="primary" type="submit">
