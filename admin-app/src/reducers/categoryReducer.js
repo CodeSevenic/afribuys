138a339d4d9dd43ab4c45072a9ea1924edeb23af
CodeSevenic <sbu.audio.production@gmail.com>
2021-05-11 13:26:40 +0200

    my changes

--- admin-app/src/reducers/categoryReducer.js
+++ admin-app/src/reducers/categoryReducer.js
@@ -6,6 +6,44 @@ const initialState = {
   error: null,
 };
 
+const buildNewCategories = (id, categories, category) => {
+  let myCategories = [];
+
+  for (let cat of categories) {
+    if (cat.parentId && cat.parentId === id) {
+      myCategories.push({
+        ...cat,
+        children:
+          cat.children && cat.children.length > 0
+            ? buildNewCategories(
+                id,
+                [
+                  ...cat.children,
+                  {
+                    _id: category._id,
+                    name: category.name,
+                    parentId: category.parentId,
+                    children: category.children,
+                  },
+                ],
+                category
+              )
+            : [],
+      });
+    } else {
+      myCategories.push({
+        ...cat,
+        children:
+          cat.children && cat.children.length > 0
+            ? buildNewCategories(id, cat.children, category)
+            : [],
+      });
+    }
+  }
+
+  return myCategories;
+};
+
 export const categoryReducer = (state = initialState, action) => {
   switch (action.type) {
     case categoryConstants.GET_ALL_CATEGORIES_SUCCESS:
@@ -17,7 +55,15 @@ export const categoryReducer = (state = initialState, action) => {
     case categoryConstants.ADD_NEW_CATEGORY_REQUEST:
       return { ...state, loading: true };
     case categoryConstants.ADD_NEW_CATEGORY_SUCCESS:
-      return { ...state, loading: false };
+      const updatedCategories = buildNewCategories(
+        state.categories,
+        action.payload.category
+      );
+      console.log(updatedCategories);
+      return {
+        ...state,
+        loading: false,
+      };
     case categoryConstants.ADD_NEW_CATEGORY_FAILURE:
       return { ...initialState };
     default:
