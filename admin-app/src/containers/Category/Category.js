efeaee2f15bebfc5e528b71f711e9acbfe564366
CodeSevenic <sbu.audio.production@gmail.com>
2021-05-11 11:08:59 +0200

    my changes

--- admin-app/src/containers/Category/Category.js
+++ admin-app/src/containers/Category/Category.js
@@ -1,16 +1,34 @@
-import React, { useEffect } from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Button, Col, Container, Modal, Row } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllCategory } from '../../actions/actionsIndex';
 import Layout from '../../components/Layout/Layout';
+import Input from '../../components/UI/Input/Input';
 
 const Category = () => {
+  const [show, setShow] = useState(false);
+
+  const [categoryName, setCategoryName] = useState('');
+  const [parentCategoryId, setParentCategoryId] = useState('');
+  const [categoryImage, setCategoryImage] = useState('');
+
   const category = useSelector((state) => state.category);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getAllCategory());
   }, [dispatch]);
+
+  const handleClose = () => {
+    const form = new FormData();
+
+    form.append('name', categoryName);
+    form.append('parentId', parentCategoryId);
+    form.append('categoryImage', categoryImage);
+
+    setShow(false);
+  };
+  const handleShow = () => setShow(true);
 
   const renderCategories = (categories) => {
     let myCategories = [];
@@ -27,6 +45,21 @@ const Category = () => {
     return myCategories;
   };
 
+  const createCategoryList = (categories, options = []) => {
+    for (let category of categories) {
+      options.push({ value: category._id, name: category.name });
+      if (category.children.length > 0) {
+        createCategoryList(category.children, options);
+      }
+    }
+
+    return options;
+  };
+
+  const handleCategoryImage = (e) => {
+    setCategoryImage(e.target.files[0]);
+  };
+
   return (
     <Layout sidebar>
       <Container>
@@ -34,7 +67,7 @@ const Category = () => {
           <Col md={12}>
             <div style={{ display: 'flex', justifyContent: 'space-between' }}>
               <h3>Category</h3>
-              <button>Add</button>
+              <button onClick={handleShow}>Add</button>
             </div>
           </Col>
         </Row>
@@ -44,6 +77,44 @@ const Category = () => {
           </Col>
         </Row>
       </Container>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add New Category</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Input
+            value={categoryName}
+            placeholder={`Category Name`}
+            onChange={(e) => setCategoryName(e.target.value)}
+          />
+          <select
+            className="form-control"
+            value={parentCategoryId}
+            onChange={(e) => setParentCategoryId(e.target.value)}
+          >
+            <option>select category</option>
+            {createCategoryList(category.categories).map((option) => (
+              <option key={option.value} value={option.value}>
+                {option.name}
+              </option>
+            ))}
+          </select>
+          <input
+            type="file"
+            name="categoryImage"
+            onChange={handleCategoryImage}
+          />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </Layout>
   );
 };
