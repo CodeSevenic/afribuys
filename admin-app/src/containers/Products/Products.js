2e8f78cf3bd4c0eb38d5609046220085041c15d2
CodeSevenic <sbu.audio.production@gmail.com>
2021-05-11 14:51:14 +0200

    my changes

--- admin-app/src/containers/Products/Products.js
+++ admin-app/src/containers/Products/Products.js
@@ -1,8 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Modal } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
 import Layout from '../../components/Layout/Layout';
+import Input from '../../components/UI/Input/Input';
 
 const Products = (props) => {
-  return <Layout sidebar>Products</Layout>;
+  const [show, setShow] = useState(false);
+
+  const [name, setName] = useState('');
+  const [quantity, setQuantity] = useState('');
+  const [price, setPrice] = useState('');
+  const [description, setDescription] = useState('');
+  const [category, setCategory] = useState('');
+  const [productPictures, setProductPictures] = useState([]);
+
+  const product = useSelector((state) => state.product);
+  const dispatch = useDispatch();
+
+  const handleClose = () => setShow(false);
+
+  const handleShow = () => setShow(true);
+
+  return (
+    <Layout sidebar>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add New Category</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Input
+            value={name}
+            placeholder={`Product Name`}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </Layout>
+  );
 };
 
 export default Products;
