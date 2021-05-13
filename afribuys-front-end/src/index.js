15bc1060c0456ad5652e6253f04d5a45c1e300c8
CodeSevenic <sbu.audio.production@gmail.com>
2021-05-13 13:08:33 +0200

    my changes

--- afribuys-front-end/src/index.js
+++ afribuys-front-end/src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import store from './store/store';
+
+window.store = store;
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
--- afribuys-front-end/src/reducers/rootReducer.js
+++ afribuys-front-end/src/reducers/rootReducer.js
@@ -1,16 +1,8 @@
 import { combineReducers } from 'redux';
-import { loginReducer } from '../reducers/authReducers';
-import { userReducer } from './userReducer';
-import { productReducer } from './productReducer';
-import { orderReducer } from './orderReducer';
 import { categoryReducer } from './categoryReducer';
 
 const rootReducer = combineReducers({
-  loginState: loginReducer,
-  user: userReducer,
   category: categoryReducer,
-  product: productReducer,
-  order: orderReducer,
 });
 
 export default rootReducer;
