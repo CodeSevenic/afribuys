933a692ecfb62e1623ec8536d149a4a3b2f847d8
CodeSevenic <sbu.audio.production@gmail.com>
2021-05-13 12:59:37 +0200

    my changes

--- afribuys-front-end/src/actions/actionsIndex.js
+++ afribuys-front-end/src/actions/actionsIndex.js
@@ -1,6 +1,1 @@
-export * from './authActions';
-export * from './userActions';
 export * from './categoryAction';
-export * from './orderActions';
-export * from './productActions';
-export * from './initialDataActions';
--- afribuys-front-end/src/actions/productActions.js
+++
@@ -1,8 +0,0 @@
-import axiosInstance from '../helpers/axios';
-
-export const addProduct = (form) => {
-  return async (dispatch) => {
-    const res = await axiosInstance.post('/product/create', form);
-    console.log(res);
-  };
-};
