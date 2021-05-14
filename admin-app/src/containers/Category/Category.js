d272ba20154739786fc25e897e96d5e35fbaf31e
CodeSevenic <sbu.audio.production@gmail.com>
2021-05-14 13:32:21 +0200

    my changes

--- admin-app/src/containers/Category/Category.js
+++ admin-app/src/containers/Category/Category.js
@@ -6,6 +6,12 @@ import Layout from '../../components/Layout/Layout';
 import Input from '../../components/UI/Input/Input';
 import NewModal from '../../components/UI/Modal/Modal';
 import CheckboxTree from 'react-checkbox-tree';
+import {
+  IoIosCheckboxOutline,
+  IoIosCheckbox,
+  IoIosArrowForward,
+  IoIosArrowDown,
+} from 'react-icons/io';
 import 'react-checkbox-tree/lib/react-checkbox-tree.css';
 
 const Category = () => {
@@ -14,6 +20,11 @@ const Category = () => {
   const [categoryName, setCategoryName] = useState('');
   const [parentCategoryId, setParentCategoryId] = useState('');
   const [categoryImage, setCategoryImage] = useState('');
+  const [checked, setChecked] = useState([]);
+  const [expanded, setExpanded] = useState([]);
+  const [checkedArray, setCheckedArray] = useState([]);
+  const [expandedArray, setExpandedArray] = useState([]);
+  const [updateCategoryModal, setUpdateCategoryModal] = useState(false);
 
   const category = useSelector((state) => state.category);
   const dispatch = useDispatch();
@@ -34,14 +45,12 @@ const Category = () => {
   const renderCategories = (categories) => {
     let myCategories = [];
     for (let category of categories) {
-      myCategories.push(
-        <li key={category.name}>
-          {category.name}
-          {category.children.length > 0 && (
-            <ul>{renderCategories(category.children)}</ul>
-          )}
-        </li>
-      );
+      myCategories.push({
+        label: category.name,
+        value: category._id,
+        children:
+          category.children.length > 0 && renderCategories(category.children),
+      });
     }
     return myCategories;
   };
@@ -61,6 +70,10 @@ const Category = () => {
     setCategoryImage(e.target.files[0]);
   };
 
+  const updateCategory = () => {
+    setUpdateCategoryModal(true);
+  };
+
   return (
     <Layout sidebar>
       <Container>
@@ -74,7 +87,27 @@ const Category = () => {
         </Row>
         <Row>
           <Col md={12}>
-            <ul>{renderCategories(category.categories)}</ul>
+            {/* <ul>{renderCategories(category.categories)}</ul> */}
+            <CheckboxTree
+              nodes={renderCategories(category.categories)}
+              checked={checked}
+              expanded={expanded}
+              onCheck={(checked) => setChecked(checked)}
+              onExpand={(expanded) => setExpanded(expanded)}
+              icons={{
+                check: <IoIosCheckbox />,
+                uncheck: <IoIosCheckboxOutline />,
+                halfCheck: <IoIosCheckboxOutline />,
+                expandClose: <IoIosArrowForward />,
+                expandOpen: <IoIosArrowDown />,
+              }}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <button>Delete</button>
+            <button onClick={updateCategory}>Edit</button>
           </Col>
         </Row>
       </Container>
@@ -82,6 +115,37 @@ const Category = () => {
         show={show}
         handleClose={handleClose}
         modalTile="Add New Category"
+      >
+        <Input
+          value={categoryName}
+          placeholder={`Category Name`}
+          onChange={(e) => setCategoryName(e.target.value)}
+        />
+        <select
+          className="form-control"
+          value={parentCategoryId}
+          onChange={(e) => setParentCategoryId(e.target.value)}
+        >
+          <option>select category</option>
+          {createCategoryList(category.categories).map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.name}
+            </option>
+          ))}
+        </select>
+        <input
+          type="file"
+          name="categoryImage"
+          onChange={handleCategoryImage}
+        />
+      </NewModal>
+
+      {/* Edit Categories */}
+      <NewModal
+        show={updateCategoryModal}
+        handleClose={() => setUpdateCategoryModal(true)}
+        modalTile="Update Categories"
+        size="lg"
       >
         <Input
           value={categoryName}
