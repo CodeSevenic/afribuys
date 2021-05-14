deb1b93f5c61352dc8c0fd5ef1fa3d9e21a967b4
CodeSevenic <sbu.audio.production@gmail.com>
2021-05-14 06:40:17 +0200

    my changes

--- afribuys-front-end/src/actions/productAction.js
+++ afribuys-front-end/src/actions/productAction.js
@@ -1,8 +1,17 @@
 import axiosInstance from '../helpers/axios';
+import { productConstants } from './constants';
 
 export const getProductBySlug = (slug) => {
   return async (dispatch) => {
     const res = await axiosInstance.get(`/products/${slug}`);
-    console.log(res);
+    if (res.status === 200) {
+      dispatch({
+        type: productConstants.GET_PRODUCTS_BY_SLUG,
+        payload: res.data,
+      });
+    } else {
+      // dispatch({
+      // })
+    }
   };
 };
