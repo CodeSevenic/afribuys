a16ac0262ac8cdf03fef7924aee9e009d38b4066
CodeSevenic <sbu.audio.production@gmail.com>
2021-05-14 07:05:30 +0200

    my changes

--- afribuys-front-end/src/containers/ProductListPage/ProductListPage.js
+++ afribuys-front-end/src/containers/ProductListPage/ProductListPage.js
@@ -12,7 +12,34 @@ const ProductListPage = (props) => {
     dispatch(getProductBySlug(match.params.slug));
   }, []);
 
-  return <Layout>ProductListPage</Layout>;
+  return (
+    <Layout>
+      <div className="card">
+        <div className="cardHeader">
+          <div>Samsung Mobile Under 10k</div>
+          <button>view all</button>
+        </div>
+        <div>
+          <div className="productContainer">
+            <div className="productImgContainer">
+              <img
+                src="http://localhost:2000/public/KKvEMah99-411ixwz8URppESrt_phone-3.png"
+                alt="Product"
+              />
+            </div>
+            <div>
+              <div>Samsung 4gb phone</div>
+              <div>
+                <span>4.3</span>
+                <span>3353</span>
+              </div>
+              <div>5000</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default ProductListPage;
