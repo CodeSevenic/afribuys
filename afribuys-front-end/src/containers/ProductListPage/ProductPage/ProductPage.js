2cf06b3ada894dd7f0fb97912774526a1a8260c3
CodeSevenic <sbu.audio.production@gmail.com>
2021-05-18 12:11:01 +0200

    my changes

--- afribuys-front-end/src/containers/ProductListPage/ProductPage/ProductPage.js
+++ afribuys-front-end/src/containers/ProductListPage/ProductPage/ProductPage.js
@@ -2,10 +2,15 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getProductPage } from '../../../actions/actionsIndex';
 import getParams from '../../../utils/getParams';
+import './ProductPage.css';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
 const ProductPage = (props) => {
   const dispatch = useDispatch();
   const product = useSelector((state) => state.product);
+  const { page } = product;
+
   useEffect(() => {
     const params = getParams(props.location.search);
     const payload = {
@@ -15,7 +20,31 @@ const ProductPage = (props) => {
     dispatch(getProductPage(payload));
   }, []);
 
-  return <>{JSON.stringify(product.page)}</>;
+  return (
+    <div className="page-content">
+      <h3>{page.title}</h3>
+      <Carousel renderThumbs={() => {}}>
+        {page.banners &&
+          page.banners.map((banner, index) => (
+            <a
+              key={index}
+              style={{ display: 'block' }}
+              href={banner.navigateTo}
+            >
+              <img src={banner.img} alt="Banner" />
+            </a>
+          ))}
+      </Carousel>
+      <div>
+        {page.products &&
+          page.products.map((product, index) => (
+            <div key={index}>
+              <img src={product.img} alt="Product" />
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default ProductPage;
