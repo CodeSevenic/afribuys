45f471eace5d3843c31afeb6d55ede1171608f14
CodeSevenic <sbu.audio.production@gmail.com>
2021-05-19 14:49:01 +0200

    my changes

--- afribuys-front-end/src/containers/CartPage/CartPage.js
+++ afribuys-front-end/src/containers/CartPage/CartPage.js
@@ -1,38 +1,53 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addToCart } from '../../actions/actionsIndex';
 import Layout from '../../components/Layout/Layout';
 import Card from '../../components/UI/Card/Card';
+import CartItem from './CartItem/CartItem';
 import './CartPage.css';
 
 const CartPage = (props) => {
   const cart = useSelector((state) => state.cart);
-  const cartItems = cart.cartItems;
+  // const cartItems = cart.cartItems;
+  const [cartItems, setCartItems] = useState(cart.cartItems);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    setCartItems(cart.cartItems);
+  }, [cart.cartItems]);
+
+  const onQuantityIncrement = (_id, qty) => {
+    // console.log({ _id, qty });
+    const { name, price, img } = cartItems[_id];
+    dispatch(addToCart({ _id, name, price, img }, 1));
+  };
+
+  const onQuantityDecrement = (_id, qty) => {
+    console.log({ _id, qty });
+    const { name, price, img } = cartItems[_id];
+    dispatch(addToCart({ _id, name, price, img }, -1));
+  };
 
   return (
     <Layout>
       <div className="cartContainer">
         <Card headerLeft="My Cart" headerRight={<div>Deliver to</div>}>
           {Object.keys(cartItems).map((key, index) => (
-            <div key={index} className="flexRow">
-              <div className="cartProductContainer">
-                <img src="" alt="Cart Product" />
-              </div>
-              <div className="cartItemDetails">
-                <div>
-                  {cartItems[key].name} - qty - {cartItems[key].qty}
-                </div>
-                <div>Delivery in 3 - 5 days</div>
-              </div>
-            </div>
+            <CartItem
+              key={index}
+              cartItem={cartItems[key]}
+              onQuantityDec={onQuantityDecrement}
+              onQuantityInc={onQuantityIncrement}
+            />
           ))}
         </Card>
         <Card
+          headerLeft="Price"
           style={{
             width: '500px',
           }}
-        >
-          Price
-        </Card>
+        ></Card>
       </div>
     </Layout>
   );
