f27dbe15517178e3ecf6ede099b56b6630e9d540
CodeSevenic <sbu.audio.production@gmail.com>
2021-05-19 03:25:32 +0200

    my changes

--- afribuys-front-end/src/App.js
+++ afribuys-front-end/src/App.js
@@ -1,9 +1,20 @@
+import { useEffect } from 'react';
 import './App.css';
 import HomePage from './containers/HomePage/HomePage';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ProductListPage from './containers/ProductListPage/ProductListPage';
+import { useDispatch, useSelector } from 'react-redux';
+import { isUserLoggedIn } from './actions/actionsIndex';
 
 function App() {
+  const auth = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (!auth.authenticate) {
+      dispatch(isUserLoggedIn());
+    }
+  }, [auth.authenticate]);
+
   return (
     <div className="App">
       <Router>
