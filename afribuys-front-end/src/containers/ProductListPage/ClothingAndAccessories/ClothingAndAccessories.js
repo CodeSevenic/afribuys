996f6e9fe04708a7ff31bfbca51152f303d95be6
CodeSevenic <sbu.audio.production@gmail.com>
2021-05-24 12:19:17 +0200

    my changes

--- afribuys-front-end/src/containers/ProductListPage/ClothingAndAccessories/ClothingAndAccessories.js
+++ afribuys-front-end/src/containers/ProductListPage/ClothingAndAccessories/ClothingAndAccessories.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { getProductBySlug } from '../../../actions/productAction';
 import Card from '../../../components/UI/Card/Card';
+import { generatePublicUrl } from '../../../urlConfig';
 import './ClothingAndAccessories.css';
 
 const ClothingAndAccessories = (props) => {
@@ -21,7 +23,25 @@ const ClothingAndAccessories = (props) => {
           padding: '10px',
           display: 'flex',
         }}
-      ></Card>
+      >
+        {product.products.map((product) => (
+          <div className="caContainer">
+            <Link
+              className="caImgContainer"
+              to={`/${product.slug}/${product._id}/p`}
+            >
+              <img
+                src={generatePublicUrl(product.productPictures[0].img)}
+                alt=""
+              />
+            </Link>
+            <div>
+              <div className="caProductName">{product.name}</div>
+              <div className="caProductPrice">R {product.price}</div>
+            </div>
+          </div>
+        ))}
+      </Card>
     </div>
   );
 };
