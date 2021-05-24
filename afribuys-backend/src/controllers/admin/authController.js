20760ef0dbebdc93f16a1c692aad0677e5d8471e
CodeSevenic <sbu.audio.production@gmail.com>
2021-05-24 13:27:04 +0200

    my changes

--- afribuys-backend/src/controllers/admin/authController.js
+++ afribuys-backend/src/controllers/admin/authController.js
@@ -38,13 +38,11 @@ exports.signup = (req, res) => {
 };
 
 exports.signin = (req, res) => {
-  User.findOne({ email: req.body.email }).exec((error, user) => {
+  User.findOne({ email: req.body.email }).exec(async (error, user) => {
     if (error) return res.status(400).json({ error });
     if (user) {
-      console.log(user.authenticate(req.body.password));
-      const isPasswordPromise = user.authenticate(req.body.password);
-      console.log(isPasswordPromise);
-      if (user.authenticate(req.body.password) && user.role === 'admin') {
+      const isPasswordPromise = await user.authenticate(req.body.password);
+      if (isPasswordPromise && user.role === 'admin') {
         const token = jwt.sign(
           { _id: user._id, role: user.role },
           process.env.JWT_SECRET,
